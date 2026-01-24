04b2f71872d75e8a6b3fd3e220dfdd32b0d124ce
Ishmael Muteremuka <imuteremuka@yahoo.com>
2026-01-24 12:34:32 +0200

    Updated Backend S3 Bucket

--- provider.tf
+++ provider.tf
@@ -1,8 +1,13 @@
 terraform {
+  required_version = "~> 1.14.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.28.0"
     }
+    random = {
+          source  = "hashicorp/random"
+          version = "~> 3.0"
+        }
   }
 }
--- s3-backend.tf
+++ s3-backend.tf
@@ -1,5 +1,22 @@
-resource "aws_s3_bucket" "im-backend-s3-backen" {
-    bucket = "im-backend-s3-backend-2024-06"
+variable "bucket_name" {
+      type        = string
+      description = "My variable used to set bucket name"
+      default     = "terraform-state-bucket"
+    }
+
+locals {
+      local_example = "This is a local variable"
+    }
+
+resource "random_id" "bucket_suffix" {
+      byte_length = 6
+    }
+resource "aws_s3_bucket" "im-s3-backend" {
+    bucket = "${var.bucket_name}-${random_id.bucket_suffix.hex}"
     
   
 }
+
+output "bucket_id" {
+    value = aws_s3_bucket.im-s3-backend.bucket
+    }
